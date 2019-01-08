7cabf41634e0cd95ffadb2c89f059a8d135b9a96
Andhurt <Andreshuertasgon@gmail.com>
2019-01-07 21:23:01 -0500

    second milestone

--- observatory/src/main/scala/observatory/Visualization.scala
+++ observatory/src/main/scala/observatory/Visualization.scala
@@ -1,11 +1,18 @@
 package observatory
 
+import scala.math.{acos, cos, pow, sin, Pi}
 import com.sksamuel.scrimage.{Image, Pixel}
+import scala.annotation.tailrec
 
 /**
   * 2nd milestone: basic visualization
   */
 object Visualization {
+
+  val EARTH_RADIUS = 6371.0
+  val P = 3.0
+  val MIN_ARC_DISTANCE = 1.0
+  val TO_RADIANS = Pi / 180.0
 
   /**
     * @param temperatures Known temperatures: pairs containing a location and the temperature at this location
@@ -13,8 +20,37 @@ object Visualization {
     * @return The predicted temperature at `location`
     */
   def predictTemperature(temperatures: Iterable[(Location, Temperature)], location: Location): Temperature = {
-    ???
+    idw(temperatures, location, P)
   }
+
+  def dist(x: Location, xi: Location): Double = {
+    val deltaLambda = ((x.lon max xi.lon) - (x.lon min xi.lon)) * TO_RADIANS
+    val sigma = acos(sin(x.lat*TO_RADIANS)*sin(xi.lat*TO_RADIANS) + cos(x.lat*TO_RADIANS)*cos(xi.lat*TO_RADIANS) * cos(deltaLambda))
+    // Convert to arc distance in km
+    EARTH_RADIUS * sigma
+  }
+
+  def idw(sample: Iterable[(Location, Temperature)], x: Location, p: Temperature) = {
+
+    @tailrec
+    def recIdw(values: Iterator[(Location, Temperature)], sumVals: Temperature, sumWeights: Temperature): Temperature = {
+      values.next match {
+        case (xi, ui) => {
+          val arc_distance = dist(x, xi)
+          if (arc_distance < MIN_ARC_DISTANCE)
+            ui
+          else {
+            val w = 1.0 / pow(arc_distance, p)
+            if (values.hasNext) recIdw(values, sumVals + w * ui, sumWeights + w)
+            else (sumVals + w * ui) / (sumWeights + w)
+          }
+        }
+      }
+    }
+
+    recIdw(sample.toIterator, 0.0, 0.0)
+  }
+
 
   /**
     * @param points Pairs containing a value and its associated color
@@ -22,7 +58,28 @@ object Visualization {
     * @return The color that corresponds to `value`, according to the color scale defined by `points`
     */
   def interpolateColor(points: Iterable[(Temperature, Color)], value: Temperature): Color = {
-    ???
+    val sortedPoints = points.toList.sortWith(_._1 < _._1).toArray
+    interpolateColor2(sortedPoints, value)
+  }
+
+  def interpolateColor2(sortedPoints: Array[(Temperature, Color)], value: Temperature): Color = {
+    for (i <- 0 until sortedPoints.length - 1) {
+      (sortedPoints(i), sortedPoints(i + 1)) match {
+        case ((v1, Color(r1, g1, b1)), (v2, Color(r2, g2, b2))) => {
+          if (v1 > value)
+            v2
+          else if (v1 <= value && v2 > value) {
+            val ratio = (value - v1) / (v2 - v1)
+            return Color(
+              ((r1 min r2) + math.abs(r2 - r1) * ratio).toInt,
+              ((g1 min g2) + math.abs(g2 - g1) * ratio).toInt,
+              ((b1 min b2) + math.abs(b2 - b1) * ratio).toInt
+            )
+          }
+        }
+      }
+    }
+    sortedPoints(sortedPoints.length-1)._2
   }
 
   /**
